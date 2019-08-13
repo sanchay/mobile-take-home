db11c131c56f83460ab28a2c2fa3e169cce27986
Sanchay <sanchay.banerjee@gmail.com>
2019-08-13 17:43:24 -0400

    Add single view project

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Sanchay on 2019-08-13.
+//  Copyright © 2019 Sanchay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
