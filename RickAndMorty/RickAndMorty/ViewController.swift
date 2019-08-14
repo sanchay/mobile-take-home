8539d2e98e70c75dfcc7f25f033ab5fa6fa3405b
Sanchay <sanchay.banerjee@gmail.com>
2019-08-14 00:53:21 -0400

    Implement Modeling and show episodes in tableview

--- RickAndMorty/RickAndMorty/ViewController.swift
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -8,15 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, Modeling {
+    
     @IBOutlet weak var tableView: UITableView!
     
+    private var episodes: Episodes?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configure()
     }
-
-
+    
+    func configure() {
+        title = "Episodes"
+        
+        self.fetchEpisodes { [weak self] episodes in
+            self?.episodes = episodes
+            self?.tableView.reloadData()
+        }
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodes?.results.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let episodeCell = tableView.dequeueReusableCell(withIdentifier: "episodeCell", for: indexPath)
+        episodeCell.textLabel?.text = episodes?.results[indexPath.row].name
+        return episodeCell
+    }
+}
