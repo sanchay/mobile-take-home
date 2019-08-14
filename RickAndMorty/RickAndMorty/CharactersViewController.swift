5f11065abde654a68edae985a1f1fde7ffb2337a
Sanchay <sanchay.banerjee@gmail.com>
2019-08-14 01:02:40 -0400

    Add class and storyboard view controller for characters

---
+++ RickAndMorty/RickAndMorty/CharactersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CharactersViewController.swift
+//  RickAndMorty
+//
+//  Created by Sanchay on 2019-08-14.
+//  Copyright © 2019 Sanchay. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
