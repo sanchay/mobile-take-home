1c4ef0a14cdd5f7536eecd6083c148bd42373dfa
Sanchay <sanchay.banerjee@gmail.com>
2019-08-14 04:02:17 -0400

    Update characters view controller and storyboard

--- RickAndMorty/RickAndMorty/CharactersViewController.swift
+++ RickAndMorty/RickAndMorty/CharactersViewController.swift
@@ -12,7 +12,9 @@ class CharactersViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var characters: [String]?
+    var characterUrls: [String]?
+    private var characters: [Character]?
+    private var filteredCharacters: [Character]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,27 +22,70 @@ class CharactersViewController: UIViewController {
         configure()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+    
     func configure() {
         title = "Characters"
+        loadCharacters()
+    }
+}
+
+extension CharactersViewController: Modeling {
+    
+    func loadCharacters() {
+        let characterIds = characterUrls?.compactMap { urlString -> String? in
+            guard let url = URL(string: urlString) else { return nil }
+            return url.lastPathComponent
+        }.joined(separator: ",")
+        guard let ids = characterIds else { return }
+        self.fetchCharacters(characterIds: ids) { [weak self] (characters) in
+            self?.characters = characters
+            self?.filterCharacters(isAlive: true)
+            self?.tableView.reloadData()
+        }
+    }
+}
+
+extension CharactersViewController {
+    
+    func filterCharacters(isAlive: Bool) {
+        filteredCharacters = characters?.filter {
+            $0.status == (isAlive ? "Alive" : "Dead")
+        }.sorted(by: >)
+    }
+    
+    @IBAction func onUpdateSwitch(_ sender: Any) {
+        filterCharacters(isAlive: (sender as! UISwitch).isOn)
+        tableView.reloadData()
     }
 }
 
 extension CharactersViewController: UITableViewDataSource {
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return characters?.count ?? 0
+        return filteredCharacters?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let characterCell = tableView.dequeueReusableCell(withIdentifier: "character_cell", for: indexPath)
+        characterCell.textLabel?.text = filteredCharacters?[indexPath.row].name
+        characterCell.detailTextLabel?.text = filteredCharacters?[indexPath.row].created
         return characterCell
     }
 }
 
 extension CharactersViewController: UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
